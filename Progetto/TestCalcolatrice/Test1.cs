58f3478b1e818473187db3e8e75c610f225c3c96
MMMaggiolo <marcello.maggiolo.14-2@accademiadigitaleliguria.it>
2025-06-25 15:59:25 +0200

    #1 sicuramente un commit che non fallirà

--- Progetto/TestCalcolatrice/Test1.cs
+++ Progetto/TestCalcolatrice/Test1.cs
@@ -8,7 +8,7 @@
         {
             CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
             double risultato = calcolatrice.Somma(2, 3);
-            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+            Assert.AreEqual(3, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
 
         }
         [TestMethod]

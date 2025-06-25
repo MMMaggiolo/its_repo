92b70ce2c1da4249af2efaf11f86a5d50cc99820
MMMaggiolo <marcello.maggiolo.14-2@accademiadigitaleliguria.it>
2025-06-25 15:32:28 +0200

    #1 Implementati cambiamenti Calcolatrice

---
+++ Progetto/CalcolatriceLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace CalcolatriceLib
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice() { }
+
+        public double Somma(double x, double y)
+        {
+
+            return x+y;
+     
+        }
+    }
+}
---
+++ Progetto/TestCalcolatrice/Test1.cs
@@ -0,0 +1,24 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+
+        }
+        [TestMethod]
+        public void TestSommaZero() { 
+
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3");
+
+        }
+
+    }
+}
